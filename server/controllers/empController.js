2dac213688cc191d6e10007192525c4b68b0309a
Aaron Rodi <metalhead5209@gmail.com>
2022-06-19 09:26:11 -0500

    fixed some styles

--- server/controllers/empController.js
+++ server/controllers/empController.js
@@ -42,23 +42,22 @@ exports.all = (req, res) => {
 
       dbConnect.getConnection((err, success) => {
         if (success) {
-          console.log(`Connected to ${process.env.DB_NAME} through DB_Port-${success.threadId}`);
-        } else if (err) 
-          console.log("ERROR", err);
-          let employee = req.body.search;
-  
-          success.query('SELECT * FROM employees WHERE first_name LIKE ?', ['%' + employee + '%'], (err, data) => {
-            success.release();
-            
-              if (data) {
-                res.render('index', { data });
-              } else {
-                console.log(err)
-              }
-              // console.log("Data from Table: \n", data)
-          });
+          console.log(`Connected to ${process.env.DB_NAME} through DB_PORT-${success.threadId}`);
+        }else if (err) console.log(err);
+        let employee = req.body.search;
+
+        success.query('SELECT * FROM employees WHERE first_name LIKE ?', ['%' + employee + '%'], (err, data) => {
+          success.release();
+
+          if (data) {
+            res.render('index', { data });
+          } else {
+            console.log(err)
+          }
+          console.log("Data from table: \n", data);
+        });
       });
-     
-    }
+      
+    };
   
-  
+ 
