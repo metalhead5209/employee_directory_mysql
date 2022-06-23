1d0f8b8a4c94fd742a347797d25dc9a62f8e5f4b
Aaron Rodi <metalhead5209@gmail.com>
2022-06-22 20:48:07 -0500

    fixed app.js syntax error

--- public/app.js
+++ public/app.js
@@ -1,5 +1,5 @@
 
-// ***** OPEN / CLOSE NEW BOOK MODAL *****
+
 const modal = document.querySelector('.modal-container')
 const addEmp = document.querySelector('#add-emp');
 const closeModal = document.querySelector('#close-modal-btn');
